27549e8352059983cf3aedc31e7718bcd89b88a0
mike <michaelwilsonsj@hotmail.com>
2020-11-12 09:41:14 -0500

    one way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { EditComponent } from './edit/edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfileComponent,
-    EditComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProfileComponent, EditComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserProfile } from '../interfaces/userprofile';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-edit',
@@ -9,9 +10,12 @@ import { UserProfile } from '../interfaces/userprofile';
   styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-  constructor(private router: Router) {}
+  userProfile: UserProfile = null;
+  constructor(private router: Router, private service: ProfileService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userProfile = this.service.getUserProfile();
+  }
 
   goToItems() {
     this.router.navigate(['/profile']);
